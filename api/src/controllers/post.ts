29f10edd344c335070e717051ad72e0e23499424
Sithija Shehara <nsithijashehara@gmail.com>
2025-02-13 22:03:30 +0530

    post controller & service

--- api/src/controllers/post.ts
+++ api/src/controllers/post.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import { createPost, getPosts, getPostsByUser } from "../services/post.service";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  getPostsByUser,
+  updatePost,
+} from "../services/post.service";
 import { Schema } from "mongoose";
 
 export const handleCreatePost = asyncHandler(
@@ -23,6 +29,24 @@ export const handleGetPostsByUser = asyncHandler(
       (req.user as { _id: Schema.Types.ObjectId })._id
     );
     res.status(200).json(posts);
+  }
+);
+
+export const handleUpdatePost = asyncHandler(
+  async (req: Request, res: Response) => {
+    const updated = await updatePost(
+      new Schema.Types.ObjectId(req.params.postId),
+      req.body
+    );
+    res.status(200).json(updated);
+  }
+);
+
+export const handleDeletePost = asyncHandler(
+  async (req: Request, res: Response) => {
+    const response = await deletePost(
+      new Schema.Types.ObjectId(req.params.postId)
+    );
   }
 );
 
--- api/src/services/post.service.ts
+++ api/src/services/post.service.ts
@@ -2,6 +2,7 @@ import { Request } from "express";
 import { IPostModel } from "../models";
 import Post from "../models/post";
 import { Schema } from "mongoose";
+import CustomError from "../utils/error.util";
 
 export const createPost = async (req: Request) => {
   const { title, content, user } = req.body;
@@ -34,5 +35,40 @@ export const getPostsByUser = async (
     return posts;
   } catch (error) {
     throw new Error("Unexpected error during fetching posts.");
+  }
+};
+
+export const updatePost = async (
+  postId: Schema.Types.ObjectId,
+  payload: Partial<IPostModel>
+): Promise<IPostModel> => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $set: payload },
+      { new: true }
+    ).populate("user");
+
+    if (!updatedPost) {
+      throw new CustomError("Post not found");
+    }
+
+    return updatedPost;
+  } catch (error) {
+    throw new Error("Unexpected error during updating post.");
+  }
+};
+
+export const deletePost = async (
+  postId: Schema.Types.ObjectId
+): Promise<void> => {
+  try {
+    const result = await Post.findByIdAndDelete(postId);
+
+    if (!result) {
+      throw new CustomError("Post not found");
+    }
+  } catch (error) {
+    throw new Error("Unexpected error during deleting post.");
   }
 };
